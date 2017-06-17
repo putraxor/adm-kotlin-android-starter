fa5627423a0635e1c9df7ee8a5b06c823e739808
Ardiansyah Putra <putraxor@gmail.com>
2017-06-17 13:07:46 +0700

    commit

---
+++ app/src/main/java/com/github/putraxor/adm/GetStarted.kt
@@ -0,0 +1,21 @@
+package com.github.putraxor.adm
+
+/**
+ * Created by putraxor on 17/06/17.
+ */
+
+//0. TODO Kotlin Basic Syntax: Define property, function, class, nullable value
+//1. TODO Kotlin DTO with Data Class
+//2. TODO Kotlin-Java Interoperable
+//3. TODO Kotlin Null Safety
+//4. TODO Kotlin Data Class
+//5. TODO Kotlin Extension Function
+//6. TODO Kotlin Smart Casting & Type Check with is
+//7. TODO Kotlin Type Alias, typealias NickName = FullName :D
+//8. TODO Kotlin lazy property, lateinit
+//9. TODO Kotlin Default & Named Parameter
+//10. TODO Kotlin Android Extension
+
+
+
+
